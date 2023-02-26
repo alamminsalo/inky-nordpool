69266d1102a3f72710ef89ef98199ce879e06ddd
Antti Lamminsalo <antti.lamminsalo@gmail.com>
2023-02-26 19:18:09 +0200

    always get spotprices

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,18 +1,8 @@
-import os
-import pandas as pd
-
 from .spotprices import get_spotprices
 from .fmi import get_fmidata
 
 def collect_df():
-    spotprices_file = '/tmp/spotprices.csv'
-    if not os.path.isfile(spotprices_file):
-        print("nordpool data: no data found, fetching...")
-        df = get_spotprices()
-        df.to_csv(spotprices_file, index=False)
-    else:
-        print("nordpool data: using cached csv")
-        df = pd.read_csv(spotprices_file, parse_dates=['timestamp'])
+    df = get_spotprices()
 
     temp = get_fmidata(df.timestamp.min(), df.timestamp.max())
     df = df.set_index('timestamp').join(temp.set_index('timestamp')).reset_index()
